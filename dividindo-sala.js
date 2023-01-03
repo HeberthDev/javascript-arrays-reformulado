50b4f597afc539283b84f4fa2d01356b4b45bc76
Heberth <heberth@onerpm.com>
2023-01-03 12:08:31 -0300

    Alterando com splice() e adição de comentários para lembrar conceitos

--- dividindo-sala.js
+++ dividindo-sala.js
@@ -1,3 +1,7 @@
+/* 
+    Slice - é uma fatia do array. Onde você pode colocar tanto de onde pra onde você quer tirar quanto à partir de onde quer tirar. Respectivamente 'alunos.slice(0, alunos.length / 2);' e 'alunos.slice(alunos.length / 2);'. 
+*/
+
 const alunos = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'Andre', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
 
 const sala1 = alunos.slice(0, alunos.length / 2);
